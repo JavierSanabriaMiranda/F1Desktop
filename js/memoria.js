49bf4831cf2daa828a2c2523d1488cd050ac6e5c
uo293758 <uo293758@uniovi.es>
2024-11-11 20:11:37 +0100

    Cambio a Imagenes de Cartas Locales
    Se ha cambiado de imágenes de cartas obtenidas de wikimedia, a las mismas imágenes en formato svg de forma local

--- js/memoria.js
+++ js/memoria.js
@@ -17,51 +17,51 @@ class Memoria {
             "cards": [
                 {
                     "element": "RedBull",
-                    "source": "https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg"
+                    "source": "multimedia/imagenes/red_bull_logo.svg"
                 },
                 {
                     "element": "RedBull",
-                    "source": "https://upload.wikimedia.org/wikipedia/de/c/c4/Red_Bull_Racing_logo.svg"
+                    "source": "multimedia/imagenes/red_bull_logo.svg"
                 },
                 {
                     "element": "McLaren",
-                    "source": "https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg"
+                    "source": "multimedia/imagenes/mclaren_logo.svg"
                 },
                 {
                     "element": "McLaren",
-                    "source": "https://upload.wikimedia.org/wikipedia/en/6/66/McLaren_Racing_logo.svg"
+                    "source": "multimedia/imagenes/mclaren_logo.svg"
                 },
                 {
                     "element": "Alpine",
-                    "source": "https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg"
+                    "source": "multimedia/imagenes/alpine_logo.svg"
                 },
                 {
                     "element": "Alpine",
-                    "source": "https://upload.wikimedia.org/wikipedia/fr/b/b7/Alpine_F1_Team_2021_Logo.svg"
+                    "source": "multimedia/imagenes/alpine_logo.svg"
                 },
                 {
                     "element": "AstonMartin",
-                    "source": "https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg"
+                    "source": "multimedia/imagenes/aston_martin_logo.svg"
                 },
                 {
                     "element": "AstonMartin",
-                    "source": "https://upload.wikimedia.org/wikipedia/fr/7/72/Aston_Martin_Aramco_Cognizant_F1.svg"
+                    "source": "multimedia/imagenes/aston_martin_logo.svg"
                 },
                 {
                     "element": "Ferrari",
-                    "source": "https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg"
+                    "source": "multimedia/imagenes/ferrari_logo.svg"
                 },
                 {
                     "element": "Ferrari",
-                    "source": "https://upload.wikimedia.org/wikipedia/de/c/c0/Scuderia_Ferrari_Logo.svg"
+                    "source": "multimedia/imagenes/ferrari_logo.svg"
                 },
                 {
                     "element": "Mercedes",
-                    "source": "https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg"
+                    "source": "multimedia/imagenes/mercedes_logo.svg"
                 },
                 {
                     "element": "Mercedes",
-                    "source": "https://upload.wikimedia.org/wikipedia/commons/f/fb/Mercedes_AMG_Petronas_F1_Logo.svg"
+                    "source": "multimedia/imagenes/mercedes_logo.svg"
                 },
             ]
         }

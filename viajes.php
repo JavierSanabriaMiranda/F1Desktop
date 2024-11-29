da6517f07d829d44ccea85f776aefa889b508d1b
uo293758 <uo293758@uniovi.es>
2024-11-29 20:52:37 +0100

    Implementación clase Carrusel de PHP para obtener imágenes de la API de Flickr

--- viajes.php
+++ viajes.php
@@ -1,6 +1,57 @@
 <!DOCTYPE HTML>
 <html lang="es">
 
+<?php
+    class Carrusel {
+
+        private $photos = array();
+        private $params = array(
+            'api_key'	=> '3caa7263ba15295bb518ee4864d92916',
+            'method'	=> 'flickr.photos.search',
+            'format'	=> 'php_serial',
+            'tags'      => 'Qatar, Doha',
+            'tag_mode'  => 'any',
+            'sort'      => 'interestingness-desc'
+        );
+
+        private function getEncodedParams() {
+            $encoded_params = array();
+            foreach ($this->params as $k => $v){
+                $encoded_params[] = urlencode($k).'='.urlencode($v);
+            }
+            return $encoded_params;
+        }
+
+        public function getPhotos() {
+            $encoded_params = $this->getEncodedParams();
+            $url = "https://api.flickr.com/services/rest/?".implode('&', $encoded_params);
+            $rsp = file_get_contents($url);
+            $rsp_obj = unserialize($rsp);
+            
+            if ($rsp_obj['stat'] == 'ok') {
+                foreach ($rsp_obj['photos']['photo'] as $photo) {
+                    if (count($this->photos) >= 10) 
+                        break;
+                    $photo_url = "https://farm" . $photo['farm'] . ".staticflickr.com/" . $photo['server'] . "/" . $photo['id'] . "_" . $photo['secret'] . ".jpg";
+                    $this->photos[] = $photo_url;
+            }
+            return $this->photos;
+            } else {
+                echo "<h3>¡Error al llamar al servicio Web de Flickr!</h3>";
+            }
+        }
+
+        public function printPhotos() {
+            foreach ($this->photos as $photo) {
+                echo "<img src=" . $photo . " alt='Imagen de Doha, Qatar'>";
+            }
+        }
+    }
+
+    $carrusel = new Carrusel();
+    $carrusel->getPhotos();
+?>
+
 <head>
     <!-- Datos que describen el documento -->
     <meta charset="UTF-8" />
@@ -39,6 +90,15 @@
     <p>Estás en <a href="index.html" title="Inicio">Inicio</a> >> Viajes</p>
 
     <h2>Viajes</h2>
+    <article>
+        <h3>Carrusel de Imágenes</h3>
+        <?php
+            $carrusel->printPhotos();
+        ?>
+        <button>&gt;</button>
+        <button>&lt;</button>
+    </article>
+
     <main>
         <button onclick="viajes.getStaticMap()">Obtener Mapa Estático</button>
         <button onclick="viajes.getDynamicMap()">Obtener Mapa Dinámico</button>

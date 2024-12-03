5687012ba88849b675781d5ba5989b05141ebd09
uo293758 <uo293758@uniovi.es>
2024-12-03 22:01:05 +0100

    Implementación de creación de base de datos y de importación de datos por medio de archivos CSV

--- php/f1_manager.php
+++ php/f1_manager.php
@@ -1,6 +1,115 @@
 <!DOCTYPE HTML>
 <html lang="es">
 
+<?php
+
+    class F1Manager {
+
+        private $server;
+        private $user;
+        private $pass;
+        private $dbname;
+        private $sql_file;
+
+        public function __construct() {
+            $this->server = "localhost";
+            $this->user = "DBUSER2024";
+            $this->pass = "DBPSWD2024";
+            $this->dbname = "f1_stats";
+            $this->sql_file = "f1_stats.sql";
+        }
+
+        public function createDatabase() {
+            try {
+                // Conexión al servidor
+                $conn = new PDO("mysql:host={$this->server}", $this->user, $this->pass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Eliminar la base de datos si existe
+                $conn->exec("DROP DATABASE IF EXISTS {$this->dbname}");
+
+                // Crear la nueva base de datos
+                $conn->exec("CREATE DATABASE IF NOT EXISTS {$this->dbname}");
+                $conn->exec("USE {$this->dbname}");
+
+                // Crear las tablas
+                $sql = file_get_contents($this->sql_file);
+                if ($sql === false) {
+                    die ("<p>Error al leer el archivo SQL</p>");
+                }
+                $conn->exec($sql);
+            } catch (PDOException $e) {
+                die ("<p>Error de conexión: " . $e->getMessage() . "</p>");
+            }
+        }
+
+        public function connect() {
+            try {
+                $conn = new PDO("mysql:host={$this->server};dbname={$this->dbname}", $this->user, $this->pass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $conn;
+            } catch (PDOException $e) {
+                die ("<p>Error de conexión: " . $e->getMessage() . "</p>");
+            }
+        }
+
+        public function importCSV() {
+            // Comprueba que el archivo sea un CSV y se haya subido correctamente
+            if (!isset($_FILES['csvFile']) || $_FILES['csvFile']['error'] !== UPLOAD_ERR_OK) {
+                die ("<p>Error al subir el archivo CSV</p>");
+            }
+
+            $csvFile = $_FILES['csvFile']['tmp_name'];
+            $conn = $this->connect();
+            try {
+                $file = fopen($csvFile, 'r');
+                if ($file === false) {
+                    die ("<p>Error al abrir el archivo CSV</p>");
+                }
+
+                $table_name = '';
+                $columns = [];
+                $query = '';
+                $stmt = null;
+
+                while (($data = fgetcsv($file)) !== false) {
+                    // Leer la primera línea para obtener el nombre de la tabla
+                    if ($data[0] === '#TABLE') {
+                        $table_name = $data[1];
+                        $columns = fgetcsv($file);
+                        
+                        if ($columns === false) {
+                            die ("<p>Error al leer los nombres de las columnas del archivo CSV</p>");
+                        }
+                        // Preparar la consulta de inserción
+                        $placeholders = implode(',', array_fill(0, count($columns), '?'));
+                        $query = "INSERT INTO $table_name (" . implode(',', $columns) . ") VALUES ($placeholders)";
+                        $stmt = $conn->prepare($query);
+                        continue;
+                    } else {
+                        // Leer el resto del archivo y ejecutar la consulta para cada fila
+                        $stmt->execute($data);
+                    }
+                }
+                fclose($file);                
+            } catch (PDOException $e) {
+                die ("<p>Error al importar datos: " . $e->getMessage() . "</p>");
+            }
+        }
+    }
+
+    // Procesar solicitud del formulario
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['createDB'])) {
+        $f1_manager = new F1Manager();
+        $f1_manager->createDatabase();
+    }
+    // Procesar importación de datos desde CSV
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['importCSV'])) {
+        $f1_manager = new F1Manager();
+        $f1_manager->importCSV();
+    }
+?>
+
 <head>
     <!-- Datos que describen el documento -->
     <meta charset="UTF-8" />
@@ -34,4 +143,19 @@
 
     <p>Estás en <a href="../index.html" title="Inicio">Inicio</a> >> <a href="../juegos.html" title="Juegos">Juegos</a> >>
         F1 Manager</p>
+
+    <h2>F1 Manager</h2>
+    <main>
+        <form method="POST">
+            <button type="submit" name="createDB">Iniciar Base de Datos</button>
+        </form>
+        <section>
+            <h4>Importar Datos de CSV:</h4>
+            <form method="POST" enctype="multipart/form-data">
+                <input type="file" name="csvFile" />
+                <button type="submit" name="importCSV">Importar</button>
+            </form>
+        </section>
+    </main>
 </body>
+</html>
